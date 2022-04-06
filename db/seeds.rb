9f98d274d38923185245a0da7dad0004426f70ab
aidenisaia <aidenisaia@gmail.com>
2022-04-06 11:52:34 -0700

    products scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,20 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Product.create!(
-    title: "Seven Mobile Apps",
-    description: "Perhaps it isn’t the prettiest interface, but it works, and we can show it to our client for approval. She can play with the other links (showing details, editing existing products, and so on). We explain to her that this is only a first step—we know it’s rough, but we wanted to get her feedback early.",
+    title: "A Good Time",
+    description: "If you want to have a good time then this is for you. We got everything you need to really enjoy yourself. Relax, sit back, and let us make the fun happen for you.",
     image_url: "E.jpg",
-    price: 23.00
+    price: 10.00
+)
+Product.create!(
+    title: "A Bad Time",
+    description: "Similar to 'A Good Time' except we make sure you have a bad time. Sadists and self sabotagers rejoice! We got you covered for a real bad time you won't forget.",
+    image_url: "hmm.jpg",
+    price: 10.00
+)
+Product.create!(
+    title: "Lean",
+    description: "You know what it is, you know you want it, and you can buy it from us. Lean.",
+    image_url: "lean.jpg",
+    price: 10.00
 )
