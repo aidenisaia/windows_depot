369b644a211f77c204e2833dfc52d08a28dd548a
aidenisaia <aidenisaia@gmail.com>
2022-04-18 16:45:00 -0700

    added migration for Orders

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,8 +42,8 @@ class ProductsController < ApplicationController
         format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
 
-        @products = Product.all.order(:title)
-        ActionCable.server.broadcast 'products', html: render_to_string('store/index', layout: false)
+        # @products = Product.all.order(:title)
+        # ActionCable.server.broadcast 'products', html: render_to_string('store/index', layout: false)
 
       else
         format.html { render :edit, status: :unprocessable_entity }
